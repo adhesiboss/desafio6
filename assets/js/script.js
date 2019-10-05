7b44a8041e690448aade61fc45668e311158ce25
raul <vrap3@hotmail.com>
2019-10-05 19:18:01 -0300

    sitio web Ricomida ejercicio Html Css Boostrap

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
+  
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+  
+  });
